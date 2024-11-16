68c9fdd7e4f69b6c173841267f6b0c901ecbd3f2
Efthalia Fakorelli <t8230156@aueb.gr>
2024-11-16 16:30:38 +0200

    ΕΒΑΛΑ ΑΜ ΕΦΗΣ

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("AM: 8230006"); 
         System.out.println("AM: 8230077");
+        System.out.println("AM: 8230156");
+        
     }
 }
