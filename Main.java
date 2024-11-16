2f8325f26fba5d557d32ccbdce045fad7017f8c3
pantliako <pliako33@gmail.com>
2024-11-16 16:23:18 +0200

    ΕΒΑΛΑ ΠΑΝΤΕΛΗ

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("AM: 8230006"); 
         System.out.println("AM: 8230077");
+        System.out.println("Am: 8230075");
     }
 }
